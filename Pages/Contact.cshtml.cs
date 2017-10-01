883f3e46e886e2170c30db262e830e66d4f7edbb
Chilezie Unachukwu <iamchilas@gmail.com>
2017-10-01 10:00:04 +0100

    chore: one punch check in

---
+++ Pages/Contact.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorDemo.Pages
+{
+    public class ContactModel : PageModel
+    {
+        public string Message { get; set; }
+
+        public Contact Contact { get; set; }
+
+        public void OnGet()
+        
+        {
+            Message = "Your contact page.";
+            Message += "...www";
+        }
+
+        public ActionResult OnPost() {
+            return RedirectToPage();
+        }
+    }
+
+    public class Contact {
+        public string Name { get;set; }
+        public string Email { get; set; }
+    }
+}
